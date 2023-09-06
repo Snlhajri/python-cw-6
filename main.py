933cca8460cd258d3957ba6bf65bdd533d33e6c4
Snlhajri <Sumianh5@gmail.com>
2023-09-06 20:03:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 # write your code here
+person = {
+    "name":"somaya",
+    "age":18,
+    "hoppies":["drawing","reading","swimming"]
+}
+
+print(person["name"])
+print(person["age"])
+
+person["age"]=17
+person["country"]="kuwait"
+print(len(person))
+person["hoppies"].append("cooking")
+def check_hobbies(person):
+    if person ["hoppies"] > "three hobbies":
+       print("wow you are amaz")
+    else:
+       print("you are not amaz")
+
+
